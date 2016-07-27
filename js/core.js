ba592451876ca1079db03ff4917420bd4d6c5e7b
Octopus <80883080@qq.com>
2016-07-27 23:40:49 +0800

    删除订单

--- js/core.js
+++ js/core.js
@@ -1,15 +1,19 @@
 function core(){
-	this.sendXhr = function(data,url,successFun){
-		if(xhr){
-			return;
-		}
-		xhr=new plus.net.XMLHttpRequest();
-		xhr.onreadystatechange=function(){
-			console.log("onreadystatechange: "+xhr.readyState);
-			successFun(xhr);
-		}
-		xhr.open( "POST", url );
-		xhr.send(JSON.stringify(data));
+	this.sendXhr = function(datas,successFun,act){
+		var keys = "account=China_Shan_Api&key=E4A96452332AA21FEA9E30B6AB7896E4&act="+act;
+		keys = md5(keys).toLowerCase();
+		console.log(keys)
+		var url ="http://103.27.5.53:8070/App/MyCenterApi.ashx?&sign="
+		+keys
+		+"&account=China_Shan_Api"
+		mui.ajax(url, {
+			data: datas,
+			dataType: 'json',
+			type: 'post',
+			timeout: 5000, //超时时间设置为5秒；
+			success: successFun
+		});
 	}
+	
 		
 }
